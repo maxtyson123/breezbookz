02ec008e23fee6619973966f36ff703d29ccfbd7
max.tyson <max.tyson@wbhs.school.nz>
2024-03-15 13:31:18 +1300

    Working Book

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,113 +1,79 @@
-import Image from "next/image";
+"use client"
+import {useEffect, useRef} from "react";
+
 
 export default function Home() {
+
+
+  const isReady = useRef(false);
+
+  // Contents
+  const bookRef = useRef<any>(null);
+
+  useEffect(() => {
+
+    // Check if the page is already loaded
+    if(isReady.current) {
+      return;
+    }
+
+    isReady.current = true;
+
+  }, []);
+
+
+  const openBook = () => {
+    console.log("openBook")
+
+    if(bookRef.current) {
+      console.log("open");
+      bookRef.current.classList.toggle("open");
+    }
+
+    // Wait one second before animating the book
+    setTimeout(() => {
+      animateBook();
+    }, 1000);
+  }
+
+  const animateBook = () => {
+    console.log("animateBook")
+    if(bookRef.current) {
+      console.log("animate");
+      bookRef.current.classList.toggle("animate");
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <>
+        <ul className="shelf">
+          <li ref={bookRef}>
+            <img onClick={openBook} className="cover" src="https://3.bp.blogspot.com/-v3Or4lDcrXU/Tn4GzPDB1jI/AAAAAAAADqE/xqHYipuz2rY/s1600/Scott%2BFitzgerald_The_Great_Gatsby_cover.jpg"/>
+            <div className="backface"></div>
+            <div className="pagecontainer">
+              <ul className="pages">
+                <li id={"cover"} className="top" style={{zIndex: 4, top: 0, left: 0}}>
+                  <h1>Title</h1>
+                  <span className="author">Author</span>
+                  <h3>Contents:</h3>
+                  <ul className="contents">
+                    <li><a href="#">Chapter 1<abbr>21/1/2014</abbr></a></li>
+                    <li><a href="#">Chapter 2<abbr>21/1/2014</abbr></a></li>
+                    <li><a href="#">Chapter 3<abbr>21/1/2014</abbr></a></li>
+                    <li><a href="#">Chapter 4<abbr>21/1/2014</abbr></a></li>
+                    <li><a href="#">Chapter 5<abbr>21/1/2014</abbr></a></li>
+                  </ul>
+                  <a href="" className="button">Continue reading</a>
+                </li>
+                <li style={{top: "2px", left: "2px", zIndex: 3}}></li>
+                <li style={{top: "4px", left: "4px", zIndex: 2}}></li>
+                <li style={{top: "6px", left: "6px", zIndex: 1}}></li>
+              </ul>
+            </div>
+            <div className="bottomcover"></div>
+          </li>
+          <h4>Click the cover to open</h4>
+        </ul>
+      </>
   );
 }
