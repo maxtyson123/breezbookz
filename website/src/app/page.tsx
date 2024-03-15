c9fd49bfd240b2d7ce511c4804acac28486c4211
Max Tyson <98maxt98@gmail.com>
2024-03-16 02:08:37 +1300

    Intro

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client"
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 
 export default function Home() {
 
 
   const isReady = useRef(false);
+  const [page, setPage] = useState(false);
 
   // Contents
   const bookRef = useRef<any>(null);
+  const fadeRef = useRef<any>(null);
 
   useEffect(() => {
 
@@ -33,6 +35,11 @@ export default function Home() {
     // Wait one second before animating the book
     setTimeout(() => {
       animateBook();
+    }, 500);
+
+    // Wait one second to change the page
+    setTimeout(() => {
+      changePage();
     }, 1000);
   }
 
@@ -44,36 +51,55 @@ export default function Home() {
     }
   }
 
+  const changePage = () => {
+
+      // Set the fade out animation
+      if(fadeRef.current) {
+        fadeRef.current.classList.toggle("active");
+      }
+
+    // Wait for the fade out animation to finish
+    setTimeout(() => {
+      setPage(true);
+    }, 800);
+
+  }
+
+
+  const bookDisplay = () => {
+    return (
+        <>
+          <ul className="shelf">
+            <li ref={bookRef}>
+              <img onClick={openBook} className="cover" src="/MainCover.webp"/>
+              <div className="backface"> <img src="/About1.webp"/></div>
+              <div className="pagecontainer">
+                <ul className="pages">
+                  <li id={"cover"} className="top" style={{zIndex: 4, top: 0, left: 0}}>
+                    <img src="/About2.webp"/>
+                  </li>
+                  <li style={{top: "2px", left: "2px", zIndex: 3}}></li>
+                  <li style={{top: "4px", left: "4px", zIndex: 2}}></li>
+                  <li style={{top: "6px", left: "6px", zIndex: 1}}></li>
+                </ul>
+              </div>
+              <div className="bottomcover"></div>
+            </li>
+            <h4>Click the cover to open</h4>
+          </ul>
+        </>
+    )
+  }
+
   return (
       <>
-        <ul className="shelf">
-          <li ref={bookRef}>
-            <img onClick={openBook} className="cover" src="https://3.bp.blogspot.com/-v3Or4lDcrXU/Tn4GzPDB1jI/AAAAAAAADqE/xqHYipuz2rY/s1600/Scott%2BFitzgerald_The_Great_Gatsby_cover.jpg"/>
-            <div className="backface"></div>
-            <div className="pagecontainer">
-              <ul className="pages">
-                <li id={"cover"} className="top" style={{zIndex: 4, top: 0, left: 0}}>
-                  <h1>Title</h1>
-                  <span className="author">Author</span>
-                  <h3>Contents:</h3>
-                  <ul className="contents">
-                    <li><a href="#">Chapter 1<abbr>21/1/2014</abbr></a></li>
-                    <li><a href="#">Chapter 2<abbr>21/1/2014</abbr></a></li>
-                    <li><a href="#">Chapter 3<abbr>21/1/2014</abbr></a></li>
-                    <li><a href="#">Chapter 4<abbr>21/1/2014</abbr></a></li>
-                    <li><a href="#">Chapter 5<abbr>21/1/2014</abbr></a></li>
-                  </ul>
-                  <a href="" className="button">Continue reading</a>
-                </li>
-                <li style={{top: "2px", left: "2px", zIndex: 3}}></li>
-                <li style={{top: "4px", left: "4px", zIndex: 2}}></li>
-                <li style={{top: "6px", left: "6px", zIndex: 1}}></li>
-              </ul>
-            </div>
-            <div className="bottomcover"></div>
-          </li>
-          <h4>Click the cover to open</h4>
-        </ul>
+        <div className={"fadeOverlay"} ref={fadeRef}></div>
+        {
+          page ?
+            <div className={"test"}>BREEZ BOOKZ WEBSITE HERE</div>
+              :
+            bookDisplay()
+        }
       </>
   );
 }
